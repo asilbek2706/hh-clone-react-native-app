2d5b0132f4afedf9267406e14ee069a175774227
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-19 21:06:28 +0500

    project themes

---
+++ src/constants/theme.js
@@ -0,0 +1,48 @@
+export const COLORS = {
+  primary: "#9b2c2c",
+  secondary: "#63171b",
+  tertiary: "#e53e3e",
+
+  gray: "#83829a",
+  gray2: "#c1c0c8",
+
+  white: "#f3f4f8",
+  lightWhite: "#fafafc",
+};
+
+export const SIZES = {
+  xSmall: 10,
+  small: 14,
+  medium: 16,
+  large: 18,
+  xLarge: 20,
+};
+
+export const SHADOWS = {
+  small: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 5,
+  },
+};
+
+export const FONTS = {
+  regular: "Roboto-Regular",
+  medium: "Roboto-Medium",
+  bold: "Roboto-Bold",
+};
